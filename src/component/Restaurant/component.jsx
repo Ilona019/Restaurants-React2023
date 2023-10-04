d6eb33d9770669962355d3163e8a3570ac53e688
Arefeva Ilona <ilona.arefieva91119@yandex.ru>
2023-10-05 00:03:24 +0300

    add component review form

--- src/component/Restaurant/component.jsx
+++ src/component/Restaurant/component.jsx
@@ -1,6 +1,7 @@
 import { Title } from '../Title/component';
 import { Menu } from '../Menu/component';
 import { Reviews } from '../Reviews/component';
+import { ReviwForm } from '../ReviewForm/component';
 import styles from './styles.module.css';
 
 export const Restaurant = ({ restaurant, className }) => {
@@ -9,6 +10,7 @@ export const Restaurant = ({ restaurant, className }) => {
             <Title name={restaurant.name} type='secondary'/>
             <Menu menu={restaurant.menu} />
             <Reviews reviews={restaurant.reviews} />
+            <ReviwForm/>
         </div>
     );
 };
---
+++ src/component/ReviewForm/component.jsx
@@ -0,0 +1,55 @@
+import { useReducer } from 'react';
+
+const DEFAULT_VALUE = {
+  name: '',
+  review: '',
+  rating: '',
+};
+
+const reducer = (state, action) => {
+  switch (action?.type) {
+    case 'setName':
+      return { ...DEFAULT_VALUE, name: action.payload };
+    case 'setReview':
+      return { ...state, review: action.payload };
+    case 'setRating':
+      return { ...state, rating: action.payload };
+    case 'reset':
+        console.log({   ...DEFAULT_VALUE});
+      return { ...DEFAULT_VALUE };
+    default:
+      return state;
+  }
+};
+
+export const ReviwForm = () => {
+  const [formValue, dispatch] = useReducer(reducer, DEFAULT_VALUE);
+
+  return (
+    <div>
+      <div>
+        <span>Name:</span>
+        <input value={formValue.name} onChange={(event) => dispatch({ type: 'setName', payload: event.target.value})} />
+      </div>
+      <div>
+        <span>Review</span>
+        <input
+          value={formValue.review}
+          onChange={(event) => {
+            dispatch({ type: 'setReview', payload: event.target.value });
+          }}
+        />
+      </div>
+      <div>
+        <span>Rating</span>
+        <input
+          value={formValue.rating}
+          onChange={(event) => {
+            dispatch({ type: 'setRating', payload: event.target.value });
+          }}
+        />
+      </div>
+      <button onClick={() => dispatch({ type: 'reset' })}>Save</button>
+    </div>
+  );
+};
