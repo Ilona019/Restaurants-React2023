c71aca7a32b72dc4c56d8b4f28194c8a9877299c
Arefeva Ilona <ilona.arefieva91119@yandex.ru>
2023-10-14 17:26:16 +0300

    added overlay on modal

--- src/component/Modal/component.jsx
+++ src/component/Modal/component.jsx
@@ -2,15 +2,18 @@ import { createPortal } from "react-dom";
 import { Button } from "../Button/component";
 import styles from "./styles.module.css";
 
-export const Modal = ({ title, children, onClose }) => {
+export const Modal = ({ title, withOverlay = true, children, onClose }) => {
   return createPortal(
-    <div className={styles.modal}>
-      <div className={styles.header}>
-        <h3 className={styles.title}> {title} </h3>
-        <Button type="close" onClick={onClose} />
+    <>
+      {withOverlay && <div className={styles.overlay}></div>}
+      <div className={styles.modal}>
+        <div className={styles.header}>
+          <h3 className={styles.title}> {title} </h3>
+          <Button type="close" onClick={onClose} />
+        </div>
+        <div className={styles.content}>{children}</div>
       </div>
-      <div className={styles.content}>{children}</div>
-    </div>,
+    </>,
     document.getElementById("modal-container")
   );
 };
